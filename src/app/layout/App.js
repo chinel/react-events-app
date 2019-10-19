9104fa085341f658bfcb0bd569e249c95fc5aff2
nelo <nelokwaz@gmail.com>
2019-10-19 14:10:08 +0100

    initial commit

---
+++ src/app/layout/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react'
+
+
+function App() {
+  return (
+    <div>
+    <h1>Revents</h1>
+    <button className="ui icon button">
+      <i className="smile icon"> </i>
+       CSS button
+    </button>
+    <Button icon="smile" content="CSS Button"/>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "semantic-ui-css/semantic.min.css";
+import './index.css';
+import App from './app/layout/App';
+import * as serviceWorker from './serviceWorker';
+
+const rootEl = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App />, rootEl);
+}
+
+if (module.hot){
+    module.hot.accept('./app/layout/App', ()=>{
+        setTimeout(render)
+    })
+}
+
+render();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
