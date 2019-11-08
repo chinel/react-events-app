ba97ecb1366589fa133469e0c12a11560076fe31
nelo <nelokwaz@gmail.com>
2019-11-08 08:06:51 +0100

    created reducers and connected the store to the application

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,4 +1,5 @@
 import {  createStore, applyMiddleware, compose} from 'redux';
+import rootReducer from '../reducers/rootReducer';
 
 
 export const configureStore = (preloadedState) => {
--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
-import './index.css';
-import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
+import { configureStore } from "./app/store/configureStore";
 
-const rootEl = document.getElementById('root');
+const store = configureStore();
+const rootEl = document.getElementById("root");
 
 let render = () => {
-    ReactDOM.render(
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>, rootEl);
-}
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
 
-if (module.hot){
-    module.hot.accept('./app/layout/App', ()=>{
-        setTimeout(render)
-    })
+if (module.hot) {
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 
 render();
