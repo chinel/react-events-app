0c4e5b3fc933ca2971438ceb9990a94e1fdf7eab
nelo <nelokwaz@gmail.com>
2019-11-08 09:07:24 +0100

    Connected a test component to the store

--- src/features/nav/NavBar/NavBar.js
+++ src/features/nav/NavBar/NavBar.js
@@ -33,6 +33,7 @@ class NavBar extends Component {
           </Menu.Item>
           
           <Menu.Item as={NavLink} to="/events" name="Events" />
+          <Menu.Item as={NavLink} to="/test" name="Test"/>
           {authenticated && <Menu.Item as={NavLink} to="/people" name="People" /> }
           {authenticated &&
           <Menu.Item>
--- src/features/testarea/TestComponent.js
+++ src/features/testarea/TestComponent.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {  connect} from 'react-redux';
+
+const mapState = (state) => ({
+    data: state.test.data   // the test represents the test reducer and the data is from the test reducer
+})
 
 class TestComponent extends Component {
     render() {
         return (
             <div>
                 <h1>Test Component</h1>
+                <p>The answer is {this.props.data}</p>
             </div>
         )
     }
 }
 
-export default TestComponent;
+export default connect(mapState)(TestComponent);
