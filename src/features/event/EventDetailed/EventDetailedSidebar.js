8d5f021c184b9bb7068dc81137c805612811378b
nelo <nelokwaz@gmail.com>
2019-11-10 13:47:37 +0100

    added the event sidebar layout structure

--- src/features/event/EventDetailed/EventDetailedSidebar.js
+++ src/features/event/EventDetailed/EventDetailedSidebar.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { Fragment, Segment, Item, Label } from 'semantic-ui-react';
 
  const EventDetailedSidebar = () => {
     return (
-        <div>
-             <h1>Event Detailed Sidebar</h1>
-        </div>
+            <Segment>
+              <Segment
+                textAlign='center'
+                style={{ border: 'none' }}
+                attached='top'
+                secondary
+                inverted
+                color='teal'
+              >
+                2 People Going
+              </Segment>
+              <Segment attached>
+                <Item.Group divided>
+                  <Item style={{ position: 'relative' }}>
+                    <Label
+                      style={{ position: 'absolute' }}
+                      color='orange'
+                      ribbon='right'
+                    >
+                      Host
+                    </Label>
+                    <Item.Image size='tiny' src='/assets/user.png' />
+                    <Item.Content verticalAlign='middle'>
+                      <Item.Header as='h3'>Attendee Name</Item.Header>
+                    </Item.Content>
+                  </Item>
+                </Item.Group>
+              </Segment>
+            </Segment>
     )
 }
 
