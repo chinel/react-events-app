d25847a4537393a10dec19e6533febf70502e1c9
nelo <nelokwaz@gmail.com>
2019-11-10 18:14:32 +0100

    added to scroll to topn restoration

--- src/features/event/EventDetailed/EventDetailedPage.js
+++ src/features/event/EventDetailed/EventDetailedPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {  connect} from 'react-redux';
 import { Grid } from "semantic-ui-react";
 import EventDetailedHeader from "./EventDetailedHeader";
 import EventDetailedInfo from "./EventDetailedInfo";
@@ -6,31 +7,21 @@ import EventDetailedChat from "./EventDetailedChat";
 import EventDetailedSidebar from "./EventDetailedSidebar";
 
 
-const event =  {
-    id: "1",
-    title: "Trip to Tower of London",
-    date: "2018-03-27",
-    category: "culture",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
-    city: "London, UK",
-    venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: "Bob",
-    hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
-    attendees: [
-      {
-        id: "a",
-        name: "Bob",
-        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
-      },
-      {
-        id: "b",
-        name: "Tom",
-        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
-      }
-    ]
-  }
-const EventDetailedPage = () => {
+const mapState = (state, ownProps) => { //Here ownProps is the props already available to the component such as the history, match, location object if routing is applied and so on and so forth
+   const eventId = ownProps.match.params.id;
+
+   let event = {};
+
+   if(eventId && state.events.length > 0){
+       event = state.events.filter(event => event.id === eventId)[0];
+   }
+
+   return{
+       event
+   }
+
+}
+const EventDetailedPage = ({event}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
@@ -45,4 +36,4 @@ const EventDetailedPage = () => {
   );
 };
 
-export default EventDetailedPage;
+export default connect(mapState)(EventDetailedPage);
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "./index.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
 import { configureStore } from "./app/store/configureStore";
+import ScrollToTop from "./app/common/util/ScrollToTop";
 
 const store = configureStore();
 const rootEl = document.getElementById("root");
@@ -15,7 +16,9 @@ let render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
+      <ScrollToTop>
         <App />
+        </ScrollToTop>
       </BrowserRouter>
     </Provider>,
     rootEl
