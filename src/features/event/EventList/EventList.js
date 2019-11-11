d1e764c0e05ef8d6d2429d9a6b8538dde5d7603f
nelo <nelokwaz@gmail.com>
2019-11-11 11:27:10 +0100

    Clean up the event list component

--- src/features/event/EventList/EventList.js
+++ src/features/event/EventList/EventList.js
@@ -3,11 +3,11 @@ import EventListItem from "./EventListItem";
 
 class EventList extends Component {
   render() {
-    const { events, onEventOpen, deleteEvent } = this.props;
+    const { events,deleteEvent } = this.props;
     return (
       <div>
         {events.map((event) => (
-          <EventListItem key={event.id} event={event} onEventOpen={onEventOpen} deleteEvent={deleteEvent}/>
+          <EventListItem key={event.id} event={event}  deleteEvent={deleteEvent}/>
         ))}
       </div>
     );
