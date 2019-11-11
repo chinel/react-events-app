15534499f40f78342288a42e975112b26daaf566
nelo <nelokwaz@gmail.com>
2019-11-11 11:28:38 +0100

    clean up the event list item component

--- src/features/event/EventList/EventListItem.js
+++ src/features/event/EventList/EventListItem.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 class EventListItem extends Component {
     render() {
-      const {event, onEventOpen, deleteEvent} = this.props;
+      const {event,deleteEvent} = this.props;
         return (
                  <Segment.Group>
                     <Segment>
