6003374678d48c5767ef8765e3cb70fe957242de
nelo <nelokwaz@gmail.com>
2019-11-24 16:26:18 +0100

    added the login and register modal to the modal manager

--- src/features/modals/ModalManager.js
+++ src/features/modals/ModalManager.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import TestModal from './TestModal';
-
+import LoginModal from './LoginModal';
+import RegisterModal from './RegisterModal';
 
 const modalLookup = {
-TestModal
+TestModal,
+LoginModal,
+RegisterModal
 }
 
 
