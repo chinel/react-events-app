3ca37558d9af9d269da70d546c2b7400b1f2e36a
nelo <nelokwaz@gmail.com>
2019-11-24 08:13:36 +0100

    created modal action creators and reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -2,6 +2,7 @@ import {  combineReducers} from 'redux';
 import { reducer as formReducer} from 'redux-form';
 import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer';
+import modalReducer from '../../features/modals/modalReducer';
 
 
 
@@ -9,7 +10,8 @@ import eventReducer from '../../features/event/eventReducer';
 const rootReducer = combineReducers({
     form: formReducer,
     test: testReducer,
-    events: eventReducer
+    events: eventReducer,
+     modalReducer: modalReducer
 })
 
 export default rootReducer;
