edd9c6ce6b238b7d65d1a391c75cd66bc159341d
nelo <nelokwaz@gmail.com>
2019-12-08 15:09:00 +0100

    created a reusable radio input

---
+++ src/app/common/form/RadioInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Form } from 'semantic-ui-react';
+
+const RadioInput = ({input, type, label,width}) => {
+    return (
+        <Form.Field>
+            <div className="ui radio">
+               <input {...input} type={type}/>{' '}{/*The first curly brackets with string that has an empty space is used to add a space in react */}
+            <label>{{label}}</label>
+            </div>
+        </Form.Field>
+    )
+}
+
+export default RadioInput
