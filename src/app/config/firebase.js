a80030428e358b3ebe164e89e258346b50a20810
nelo <nelokwaz@gmail.com>
2019-12-09 12:47:02 +0100

    ignored  config

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    /*Add your firebase config here */
+    apiKey: "AIzaSyB2TIUe9_mOC525BlWpOnXp1WOG5mDIuWA",
+    authDomain: "revents-53fed.firebaseapp.com",
+    databaseURL: "https://revents-53fed.firebaseio.com",
+    projectId: "revents-53fed",
+    storageBucket: "revents-53fed.appspot.com",
+    messagingSenderId: "141473127856",
+    appId: "1:141473127856:web:407c969683c1f4b10dca75",
+    measurementId: "G-Z2BX2E433Q"
 }
 
 firebase.initializeApp(firebaseConfig);
