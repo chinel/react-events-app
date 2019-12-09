4010e8c86930d3928bed46d3786f1eb5406fbc15
nelo <nelokwaz@gmail.com>
2019-12-09 12:42:18 +0100

    added firebase config

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -2,14 +2,7 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB2TIUe9_mOC525BlWpOnXp1WOG5mDIuWA",
-    authDomain: "revents-53fed.firebaseapp.com",
-    databaseURL: "https://revents-53fed.firebaseio.com",
-    projectId: "revents-53fed",
-    storageBucket: "revents-53fed.appspot.com",
-    messagingSenderId: "141473127856",
-    appId: "1:141473127856:web:407c969683c1f4b10dca75",
-    measurementId: "G-Z2BX2E433Q"
+    /*Add your firebase config here */
 }
 
 firebase.initializeApp(firebaseConfig);
