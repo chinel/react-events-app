086cc7c5b85fc32cd5dfa23953b43d9f17fbb351
nelo <nelokwaz@gmail.com>
2019-12-14 07:31:26 +0100

    separated and added the user detailed description page

---
+++ src/features/user/UserDetailed/UserDetailedDescription.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Grid, Segment, Header, Item, Icon, List } from 'semantic-ui-react';
+
+const UserDetailedDescription = () => {
+    return (
+        <Grid.Column width={12}>
+                    <Segment>
+                        <Grid columns={2}>
+                            <Grid.Column width={10}>
+                                <Header icon='smile' content='About Display Name'/>
+                                <p>I am a: <strong>Occupation Placeholder</strong></p>
+                                <p>Originally from <strong>United Kingdom</strong></p>
+                                <p>Member Since: <strong>28th March 2018</strong></p>
+                                <p>Description of user</p>
+
+                            </Grid.Column>
+                            <Grid.Column width={6}>
+
+                                <Header icon='heart outline' content='Interests'/>
+                                <List>
+                                    <Item>
+                                        <Icon name='heart'/>
+                                        <Item.Content>Interest 1</Item.Content>
+                                    </Item>
+                                    <Item>
+                                        <Icon name='heart'/>
+                                        <Item.Content>Interest 2</Item.Content>
+                                    </Item>
+                                    <Item>
+                                        <Icon name='heart'/>
+                                        <Item.Content>Interest 3</Item.Content>
+                                    </Item>
+                                </List>
+                            </Grid.Column>
+                        </Grid>
+
+                    </Segment>
+                </Grid.Column>
+              
+    )
+}
+
+export default UserDetailedDescription
--- src/features/user/UserDetailed/UserDetailedPage.js
+++ src/features/user/UserDetailed/UserDetailedPage.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {Button, Card, Grid, Header, Icon, Image, Item, List, Menu, Segment} from "semantic-ui-react";
 import UserDetailedEvents from './UserDetailedEvents';
 import UserDetailedPhotos from './UserDetailedPhotos';
+import UserDetailedSidebar from './UserDetailedSidebar';
+import UserDetailedDescription from './UserDetailedDescription';
 
 class UserDetailedPage extends Component {
 
@@ -26,40 +28,8 @@ class UserDetailedPage extends Component {
 
                     </Segment>
                 </Grid.Column>
-                <Grid.Column width={12}>
-                    <Segment>
-                        <Grid columns={2}>
-                            <Grid.Column width={10}>
-                                <Header icon='smile' content='About Display Name'/>
-                                <p>I am a: <strong>Occupation Placeholder</strong></p>
-                                <p>Originally from <strong>United Kingdom</strong></p>
-                                <p>Member Since: <strong>28th March 2018</strong></p>
-                                <p>Description of user</p>
-
-                            </Grid.Column>
-                            <Grid.Column width={6}>
-
-                                <Header icon='heart outline' content='Interests'/>
-                                <List>
-                                    <Item>
-                                        <Icon name='heart'/>
-                                        <Item.Content>Interest 1</Item.Content>
-                                    </Item>
-                                    <Item>
-                                        <Icon name='heart'/>
-                                        <Item.Content>Interest 2</Item.Content>
-                                    </Item>
-                                    <Item>
-                                        <Icon name='heart'/>
-                                        <Item.Content>Interest 3</Item.Content>
-                                    </Item>
-                                </List>
-                            </Grid.Column>
-                        </Grid>
-
-                    </Segment>
-                </Grid.Column>
-              
+                <UserDetailedDescription/>
+                <UserDetailedSidebar/>
                <UserDetailedPhotos/>
                <UserDetailedEvents/>
             </Grid>
