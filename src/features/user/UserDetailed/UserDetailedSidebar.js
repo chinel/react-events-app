3917b638f3d6dd0f07d0f2aa226d26f428bde3cc
nelo <nelokwaz@gmail.com>
2019-12-14 08:18:48 +0100

    completed the user detailed sidebar component

--- src/features/user/UserDetailed/UserDetailedSidebar.js
+++ src/features/user/UserDetailed/UserDetailedSidebar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {  Grid, Segment, Button} from 'semantic-ui-react';
+import {  Link} from 'react-router-dom';
 
 const UserDetailedSidebar = () => {
     return (
         <Grid.Column width={4}>
         <Segment>
-            <Button color='teal' fluid basic content='Edit Profile'/>
+            <Button as={Link} to="/settings" color='teal' fluid basic content='Edit Profile'/>
         </Segment>
     </Grid.Column>
 
