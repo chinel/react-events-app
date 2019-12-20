bd579c63f9bd78893715a050b76dd2524446a8fb
nelo <nelokwaz@gmail.com>
2019-12-20 18:18:53 +0100

    added links to the profile images and event title

--- src/features/event/EventDetailed/EventDetailedSidebar.js
+++ src/features/event/EventDetailed/EventDetailedSidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Segment, Item, Label, List } from "semantic-ui-react";
+import { Link } from 'react-router-dom';
 
 const EventDetailedSidebar = ({ attendees }) => {
     const isHost = false;
@@ -31,7 +32,7 @@ const EventDetailedSidebar = ({ attendees }) => {
                   }
                 <Item.Image size="tiny" src={attendee.photoURL} />
                 <Item.Content verticalAlign="middle">
-                  <Item.Header as="h3"><a>{attendee.displayName}</a></Item.Header>
+                  <Item.Header as="h3"><Link to={`/profile/${attendee.id}`}>{attendee.displayName}</Link></Item.Header>
                 </Item.Content>
               </Item>
             ))}
--- src/features/event/EventList/EventListItem.js
+++ src/features/event/EventList/EventListItem.js
@@ -3,6 +3,7 @@ import { Segment, Item, Icon, List, Button, Label } from "semantic-ui-react";
 import format from "date-fns/format";
 import EventListAttendee from "./EventListAttendee";
 import { Link } from "react-router-dom";
+import { objectToArray } from "../../../app/common/util/helpers";
 
 class EventListItem extends Component {
   render() {
@@ -14,9 +15,9 @@ class EventListItem extends Component {
             <Item>
               <Item.Image size="tiny" circular src={event.hostPhotoURL} />
               <Item.Content>
-                <Item.Header as="a">{event.title}</Item.Header>
+                <Item.Header as={Link} to={`/event/${event.id}`}>{event.title}</Item.Header>
                 <Item.Description>
-                  Hosted by <a>{event.hostedBy}</a>
+                  Hosted by <Link to={`/profile/${event.hostUid}`}>{event.hostedBy}</Link>
                 </Item.Description>
                 {event.cancelled &&
                 <Label style={{top: '-40px'}} color="red" ribbon="right" content="This event has been cancelled"/>}
@@ -34,8 +35,8 @@ class EventListItem extends Component {
         <Segment secondary>
           <List horizontal>
             {event.attendees && /*The object.values converts the object data to an array */
-              Object.values(event.attendees).map((attendee, index)=> (
-                <EventListAttendee key={index} attendee={attendee} />
+              objectToArray(event.attendees).map((attendee)=> (
+                <EventListAttendee key={attendee.id} attendee={attendee} />
               ))}
           </List>
         </Segment>
