5f13b1432af5d86442846f87abc1e2ab4caec9c8
nelo <nelokwaz@gmail.com>
2019-12-20 19:13:16 +0100

    added the functionality to view other users profile

--- src/features/user/UserDetailed/UserDetailedPage.js
+++ src/features/user/UserDetailed/UserDetailedPage.js
@@ -1,31 +1,38 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import { firestoreConnect } from 'react-redux-firebase';
+import { firestoreConnect, isEmpty } from 'react-redux-firebase';
 import {Grid} from "semantic-ui-react";
 import UserDetailedEvents from './UserDetailedEvents';
 import UserDetailedPhotos from './UserDetailedPhotos';
 import UserDetailedSidebar from './UserDetailedSidebar';
 import UserDetailedDescription from './UserDetailedDescription';
 import UserDetailedHeader from './UserDetailedHeader';
+import { userDetaileQuery } from '../userQueries';
 
 
-const query = ({auth}) =>  {
-    return [
-        {
-          collection: "users",
-          doc: auth.uid,
-          subcollections: [{ collection: "photos" }],
-          storeAs: "photos"
-        }
-      ];  
-}
 
-const mapState = (state) => ({
+
+const mapState = (state, ownProps) => {
+  let userUid = null;
+  let profile = {};
+
+  if(ownProps.match.params.id === state.auth.uid){
+
+   profile= state.firebase.profile
+
+  }
+  else{
+   profile = !isEmpty(state.firestore.ordered.profile) && state.firestore.ordered.profile[0]
+   userUid = ownProps.match.params.id
+
+  }
+return {
   auth: state.firebase.auth,
-  profile: state.firebase.profile,
+  profile, //this is the same as profile: profile
+  userUid,
   photos: state.firestore.ordered.photos
-});
+}};
 
 
 class UserDetailedPage extends Component {
@@ -48,4 +55,4 @@ class UserDetailedPage extends Component {
     }
 }
 
-export default compose(connect(mapState), firestoreConnect(auth => query(auth)))(UserDetailedPage);
+export default compose(connect(mapState), firestoreConnect((auth, userUid )=> userDetaileQuery(auth, userUid)))(UserDetailedPage);
