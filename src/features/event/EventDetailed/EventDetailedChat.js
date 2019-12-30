7fab70171bc771c78f5893171c2b04e6b0e1e029
nelo <nelokwaz@gmail.com>
2019-12-30 05:26:19 +0100

    Displayed the first step of the events comment on the ui

--- src/features/event/EventDetailed/EventDetailedChat.js
+++ src/features/event/EventDetailed/EventDetailedChat.js
@@ -1,42 +1,50 @@
-import React from 'react';
-import { Segment, Header, Comment } from 'semantic-ui-react';
-import EventDetailedChatForm from './EventDetailedChatForm';
+import React from "react";
+import { Segment, Header, Comment } from "semantic-ui-react";
+import EventDetailedChatForm from "./EventDetailedChatForm";
+import { Link } from "react-router-dom";
+import distanceInWords from "date-fns/distance_in_words";
 
-const EventDetailedChat = ({addEventComment, eventId}) => {
-    return (
-            <div>
-              <Segment
-                textAlign="center"
-                attached="top"
-                inverted
-                color="teal"
-                style={{ border: 'none' }}
-              >
-                <Header>Chat about this event</Header>
-              </Segment>
-        
-              <Segment attached>
-                <Comment.Group>
-                  <Comment>
-                    <Comment.Avatar src="/assets/user.png" />
-                    <Comment.Content>
-                      <Comment.Author as="a">Matt</Comment.Author>
-                      <Comment.Metadata>
-                        <div>Today at 5:42PM</div>
-                      </Comment.Metadata>
-                      <Comment.Text>How artistic!</Comment.Text>
-                      <Comment.Actions>
-                        <Comment.Action>Reply</Comment.Action>
-                      </Comment.Actions>
-                    </Comment.Content>
-                  </Comment>
-        
-                  
-                </Comment.Group>
-                <EventDetailedChatForm addEventComment={addEventComment} eventId={eventId}/>
-              </Segment>
-            </div>
-    )
-}
+const EventDetailedChat = ({ addEventComment, eventId, eventChat }) => {
+  return (
+    <div>
+      <Segment
+        textAlign="center"
+        attached="top"
+        inverted
+        color="teal"
+        style={{ border: "none" }}
+      >
+        <Header>Chat about this event</Header>
+      </Segment>
 
-export default EventDetailedChat
+      <Segment attached>
+        <Comment.Group>
+          {eventChat &&
+            eventChat.map(comment => (
+              <Comment key={comment.id}>
+                <Comment.Avatar src={comment.photoURL || "/assets/user.png"} />
+                <Comment.Content>
+                  <Comment.Author as={Link} to={`/profile/${comment.uid}`}>
+                    {comment.displayName}
+                  </Comment.Author>
+                  <Comment.Metadata>
+                    <div>{distanceInWords(comment.date, Date.now())} ago</div>
+                  </Comment.Metadata>
+                  <Comment.Text>{comment.text}</Comment.Text>
+                  <Comment.Actions>
+                    <Comment.Action>Reply</Comment.Action>
+                  </Comment.Actions>
+                </Comment.Content>
+              </Comment>
+            ))}
+        </Comment.Group>
+        <EventDetailedChatForm
+          addEventComment={addEventComment}
+          eventId={eventId}
+        />
+      </Segment>
+    </div>
+  );
+};
+
+export default EventDetailedChat;
--- src/features/event/EventDetailed/EventDetailedPage.js
+++ src/features/event/EventDetailed/EventDetailedPage.js
@@ -1,85 +1,100 @@
-import React, { Component } from 'react'
-import {  connect} from 'react-redux';
-import { withFirestore, firebaseConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withFirestore, firebaseConnect, isEmpty } from "react-redux-firebase";
+import { compose } from "redux";
 import { Grid } from "semantic-ui-react";
 import EventDetailedHeader from "./EventDetailedHeader";
 import EventDetailedInfo from "./EventDetailedInfo";
 import EventDetailedChat from "./EventDetailedChat";
 import EventDetailedSidebar from "./EventDetailedSidebar";
-import { toastr } from 'react-redux-toastr';
-import { objectToArray } from '../../../app/common/util/helpers';
-import {  goingToEvent, cancelGoingToEvent } from '../../user/userActions';
-import { addEventComment } from '../eventActions';
+import { toastr } from "react-redux-toastr";
+import { objectToArray } from "../../../app/common/util/helpers";
+import { goingToEvent, cancelGoingToEvent } from "../../user/userActions";
+import { addEventComment } from "../eventActions";
 
+const mapState = (state, ownProps) => {
+  //Here ownProps is the props already available to the component such as the history, match, location object if routing is applied and so on and so forth
+  //const eventId = ownProps.match.params.id;   we would no longer be needing to use the match inside of here as it is used in the componentDidMount react lifecycle method already
 
-const mapState = (state,/*  ownProps */) => { //Here ownProps is the props already available to the component such as the history, match, location object if routing is applied and so on and so forth
-   //const eventId = ownProps.match.params.id;   we would no longer be needing to use the match inside of here as it is used in the componentDidMount react lifecycle method already
+  let event = {};
 
-   let event = {};
+  if (state.firestore.ordered.events && state.firestore.ordered.events[0]) {
+    //event = state.events.filter(event => event.id === eventId)[0];
+    event = state.firestore.ordered.events[0];
+  }
 
-   if(state.firestore.ordered.events && state.firestore.ordered.events[0]){
-       //event = state.events.filter(event => event.id === eventId)[0];
-       event = state.firestore.ordered.events[0];
-   }
-
-   return{
-       event,
-       auth: state.firebase.auth
-   }
-
-}
+  return {
+    event,
+    auth: state.firebase.auth,
+    eventChat:
+      !isEmpty(state.firebase.data.event_chat) &&
+      objectToArray(state.firebase.data.event_chat[ownProps.match.params.id])
+  };
+};
 
 const actions = {
   goingToEvent,
   cancelGoingToEvent,
   addEventComment
-}
-
+};
 
 class EventDetailedPage extends Component {
-
- async componentDidMount(){
-   const {firestore, match /* history */} = this.props;
+  async componentDidMount() {
+    const { firestore, match /* history */ } = this.props;
     await firestore.setListener(`events/${match.params.id}`);
-  /*  console.log(event);
+    /*  console.log(event);
    if(!event.exists){
     history.push('/events');
     toastr.error("Sorry", "Event Not Found");
    } */
-
   }
 
-   async componentWillMount (){
-    const {firestore, match /* history */} = this.props;
+  async componentWillMount() {
+    const { firestore, match /* history */ } = this.props;
     await firestore.unsetListener(`events/${match.params.id}`);
   }
 
-
   render() {
-    const {event, auth, goingToEvent, cancelGoingToEvent,addEventComment} = this.props;
-    const attendees = event && event.attendees && objectToArray(event.attendees);//this check to see if there are events and if there are also attendees under the events then it uses the objectToArray helper method to convert it to an array
+    const {
+      event,
+      auth,
+      goingToEvent,
+      cancelGoingToEvent,
+      addEventComment,
+      eventChat
+    } = this.props;
+    const attendees =
+      event && event.attendees && objectToArray(event.attendees); //this check to see if there are events and if there are also attendees under the events then it uses the objectToArray helper method to convert it to an array
     const isHost = event.hostUid === auth.uid;
     const isGoing = attendees && attendees.some(a => a.id === auth.uid); //this check to see if attendees is present and if attendees has an id matching auth id it returns true or false    return (
-     return(
-     <Grid>
-      <Grid.Column width={10}>
-        <EventDetailedHeader  event={event} isHost={isHost} isGoing={isGoing} goingToEvent={goingToEvent} cancelGoingToEvent={cancelGoingToEvent}/>
-        <EventDetailedInfo event={event} />
-        <EventDetailedChat addEventComment={addEventComment} eventId={event.id} />
-      </Grid.Column>
-      <Grid.Column width={6}>
-        <EventDetailedSidebar attendees={attendees}/>
-      </Grid.Column>
-    </Grid>
-    )
+    return (
+      <Grid>
+        <Grid.Column width={10}>
+          <EventDetailedHeader
+            event={event}
+            isHost={isHost}
+            isGoing={isGoing}
+            goingToEvent={goingToEvent}
+            cancelGoingToEvent={cancelGoingToEvent}
+          />
+          <EventDetailedInfo event={event} />
+          <EventDetailedChat
+            addEventComment={addEventComment}
+            eventId={event.id}
+            eventChat={eventChat}
+          />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <EventDetailedSidebar attendees={attendees} />
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
-
-
 //the withFirestore Higher order component provides us with firebase and firestore methods that we can use to do a whole lot
-export default compose(withFirestore,
+export default compose(
+  withFirestore,
   connect(mapState, actions),
-  firebaseConnect((props) => ([`event_chat/${props.match.params.id}`]))
+  firebaseConnect(props => [`event_chat/${props.match.params.id}`])
 )(EventDetailedPage);
