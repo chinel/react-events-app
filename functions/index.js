c1eed78bea310c351ff8746c7fd1bfc30fcd8644
nelo <nelokwaz@gmail.com>
2019-12-31 07:07:19 +0100

    created the cloud function to create an activity on cancel of an event

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,19 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
+const newActivity = (type, event, id) => {
+  return {
+    type: type,
+    eventDate: event.date,
+    hostedBy: event.hostedBy,
+    title: event.title,
+    photoURL: event.hostPhotoURL,
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    hostUid: event.hostUid,
+    eventId: id
+  };
+};
+
 //OUR FIRST FUNCTION
 //exports which we will be needing anytime we want to create a cloud function
 //createActivity is the name of the function
@@ -12,30 +25,57 @@ exports.createActivity = functions.firestore
     let newEvent = event.data(); //this gives us access to the event document that is being created
     console.log(newEvent); // logging out in the console helps us diagnose if we have an error as cloud error reporting won;t let you know where the exact error is
 
-    const activity = {
-        type: 'newEvent',
-        eventDate: newEvent.date,
-        hostedBy: newEvent.hostedBy,
-        title: newEvent.title,
-        photoURL: newEvent.hostPhotoURL,
-        timestamp: admin.firestore.FieldValue.serverTimestamp(),
-        hostUid: newEvent.hostUid,
-        eventId: event.id
-    }
+    const activity = newActivity("newEvent", newEvent, event.id);
 
     console.log(activity);
 
-    return admin.firestore().collection('activity')
-    .add(activity)//this will add the activity and create a random id
-    .then(docRef => {// the javascript we will be using the create cloud functions does not give us access to async so we will be using then promise chain
+    return admin
+      .firestore()
+      .collection("activity")
+      .add(activity) //this will add the activity and create a random id
+      .then(docRef => {
+        // the javascript we will be using the create cloud functions does not give us access to async so we will be using then promise chain
         return console.log("Activity created with ID: ", docRef.id);
-    }).catch(err => {
-        return console.log('Error adding activity', err);// the error reporting for cloud function is not reliable so we have to make use of console.logs to always view whatever information at any point in time
-    })
-});
+      })
+      .catch(err => {
+        return console.log("Error adding activity", err); // the error reporting for cloud function is not reliable so we have to make use of console.logs to always view whatever information at any point in time
+      });
+  });
 
+exports.cancelActivity = functions.firestore
+  .document("events/{eventId}")
+  .onUpdate((event, context) => {
+    let updatedEvent = event.after.data();
+    let previousEventData = event.before.data();
+    console.log({ event }); // surrounding a variable with curly bracket here on output shows the title of the variable then the value of the variable, it helps to easily locate the variable when you have other console.logs
+    console.log({ context });
+    console.log({ updatedEvent });
+    console.log({ previousEventData });
 
+    if (
+      !updatedEvent.cancelled ||
+      updatedEvent.cancelled === previousEventData.cancelled
+    )
+      return false;
 
+    const activity = newActivity(
+      "cancelledEvent",
+      updatedEvent,
+      context.params.eventId
+    );
+    console.log(activity);
+    return admin
+      .firestore()
+      .collection("activity")
+      .add(activity) //this will add the activity and create a random id
+      .then(docRef => {
+        // the javascript we will be using the create cloud functions does not give us access to async so we will be using then promise chain
+        return console.log("Activity created with ID: ", docRef.id);
+      })
+      .catch(err => {
+        return console.log("Error adding activity", err); // the error reporting for cloud function is not reliable so we have to make use of console.logs to always view whatever information at any point in time
+      });
+  });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
