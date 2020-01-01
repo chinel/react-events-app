0a79d1c4cbc72ef4c029f21b41b047e33f02c416
nelo <nelokwaz@gmail.com>
2020-01-01 07:53:23 +0100

    Added the following and followers to the interface

--- src/features/user/PeopleDashboard/PeopleDashboard.js
+++ src/features/user/PeopleDashboard/PeopleDashboard.js
@@ -1,27 +1,50 @@
-import React from 'react';
-import { Grid, Segment, Header, Card } from 'semantic-ui-react';
-import PersonCard from './PersonCard';
+import React from "react";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { Grid, Segment, Header, Card } from "semantic-ui-react";
+import PersonCard from "./PersonCard";
 
-const PeopleDashboard = () => {
+const query = ({ auth }) => {
+  return [
+    {
+      collection: "users",
+      doc: auth.uid,
+      subcollections: [{ collection: "following" }],
+      storeAs: "following"
+    },
+    {
+      collection: "users",
+      doc: auth.uid,
+      subcollections: [{ collection: "followers" }],
+      storeAs: "followers"
+    }
+  ];
+};
+
+const mapState = state => ({
+  followings: state.firestore.ordered.following,
+  followers: state.firestore.ordered.followers,
+  auth: state.firebase.auth
+});
+
+const PeopleDashboard = ({ followers, followings }) => {
   return (
     <Grid>
       <Grid.Column width={16}>
         <Segment>
           <Header dividing content="People following me" />
           <Card.Group itemsPerRow={8} stackable>
-            <PersonCard />
-            <PersonCard />
-            <PersonCard />
-            <PersonCard />
-            <PersonCard />
+            {followers && followers.map(follower => <PersonCard  key={follower.id} user={follower}/>)}
           </Card.Group>
         </Segment>
         <Segment>
           <Header dividing content="People I'm following" />
           <Card.Group itemsPerRow={8} stackable>
-            <PersonCard />
-            <PersonCard />
-            <PersonCard />
+            {followings && followings.map(following => (
+            <PersonCard key={following.id} user={following} />     
+            ))}
+           
           </Card.Group>
         </Segment>
       </Grid.Column>
@@ -29,4 +52,4 @@ const PeopleDashboard = () => {
   );
 };
 
-export default PeopleDashboard;
+export default compose(connect(mapState), firestoreConnect(props => query(props)))(PeopleDashboard);
